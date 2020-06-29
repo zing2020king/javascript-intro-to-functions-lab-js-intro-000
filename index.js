3e8b55df03854934796e3e86309ce718d7785bf7
Tenzing Shrestha <tenzingtheking85@gmail.com>
2020-06-29 14:37:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase()===string);
    return "I can't hear you!";
 }
+function sayHiToGrandma(string) {
+  if(string.toUpperCase()=== string);
+    return "YES INDEED!";
+}
